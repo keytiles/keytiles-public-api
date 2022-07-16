3d5250d4f0d3ff6e342dd682101228d4e4e89401
attilaw <attila.wind@keytiles.com>
2022-07-16 16:26:35 +0200

    initial compiling version

---
+++ src/test/java/JustATest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import com.keytiles.api.model.common.v2.ContainerQueryRangeResponseClass;
+
+public class JustATest {
+
+	@Test
+	public void modelTest() {
+		ContainerQueryRangeResponseClass response = null;
+	}
+}
