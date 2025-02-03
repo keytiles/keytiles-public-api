eedc6610bec6bc9d172740935912501c0a92fb25
AttilaW <attila.wind@keytiles.com>
2025-02-03 10:00:24 +0100

    fixing mvn-deploy a bit

--- mvn-deploy.sh
+++ mvn-deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 mvn clean source:jar deploy
+
+echo "NOTE: for some reason mvn deploy re-generated the classes the way now Constructor comments are missing... You see diff now in Git... Just execute 'mvn generate-sources' now to get back comments - until we find wh this is happening..."
